851578f21c57e55d18cd69e5323df7e90541c9aa
Sergej080295 <milowanow-sergej@mail.ru>
2023-12-21 21:30:28 +0300

    Lesson 1 task 1 come..

--- Lesson_1/1_1/Program.cs
+++ Lesson_1/1_1/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
 
+
 // 1. Напишите программу, которая на вход принимает два числа и проверяет,
 //    является ли первое число квадратом второго.
 

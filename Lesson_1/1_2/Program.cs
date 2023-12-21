3acac698343c306d37748266cb056a00bcc8713c
Sergej080295 <milowanow-sergej@mail.ru>
2023-12-21 21:55:27 +0300

    Сделал 2-е задание

---
+++ Lesson_1/1_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+// 2. Напишите программу, которая на вход принимает одно число
+//    (N), а на выходе показывает все целые числа в промежутке от -N до N.
+
+
+        Console.Write("Введите число N: ");
+        int N = Convert.ToInt32(Console.ReadLine());
+        int startNumber = -N;
+        int endNumber = N;
+        int currentNumber = startNumber;
+
+        while (currentNumber <= endNumber)
+        {
+            Console.Write(currentNumber + " ");
+            currentNumber++;
+        }

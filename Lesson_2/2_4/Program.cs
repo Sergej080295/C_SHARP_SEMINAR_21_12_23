bb287531a751c89c60adaf4c2467ecc63234dfb8
Sergej080295 <milowanow-sergej@mail.ru>
2023-12-25 21:56:57 +0300

    Были добавлены 4 задачи по 2 семинару

---
+++ Lesson_2/2_4/Program.cs
@@ -0,0 +1,16 @@
+﻿// 4. Напишите программу, которая выводит третью с конца
+//    цифру заданного числа или сообщает, что третьей цифры нет.
+
+    Console.WriteLine("Введите число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    int num1 = (number / 100) % 10;
+
+    if (num1 != 0)
+    {
+        Console.WriteLine($"Третья цифра с конца: {num1}");
+    }
+    else
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }

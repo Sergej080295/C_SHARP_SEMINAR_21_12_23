9e720f435697b9e7dc1d893f5b06c4e6b4490f26
Sergej080295 <milowanow-sergej@mail.ru>
2024-01-11 21:49:04 +0300

    Добавил решение 3-х задач по 3-му семинару

---
+++ Lesson_3/3_3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найдите произведения пар чисел в одномерном массиве. Парой
+// считаем первый и последний элемент, второй и предпоследний и
+// т.д. Результат запишите в новый массив. 
+        
+        int[] numbers = { 10, 8, 6, 4, 2, 5, 25, 1, 3, 5, 7, 9 };
+        int[] res = new int[numbers.Length / 2];
+
+        for (int i = 0; i < numbers.Length / 2; i++)
+        {
+            res[i] = numbers[i] * numbers[numbers.Length - 1 - i];
+            Console.WriteLine("Результат: " + res[i]);
+        }

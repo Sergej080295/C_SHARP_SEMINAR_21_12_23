5fa8d50bb260c3b0b5519c7c7483493880b9fd53
Sergej080295 <milowanow-sergej@mail.ru>
2024-01-17 20:29:19 +0300

    Добавил решение задач по 4 семинару

---
+++ Lesson_4/4_2/Program.cs
@@ -0,0 +1,65 @@
+﻿// 2. Задайте массив из N случайных целых
+//    чисел (N вводится с клавиатуры).
+//    Найдите количество чисел, которые
+//    оканчиваются на 1 и делятся нацело на 7.
+
+using System;
+
+namespace My_4_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            // ... (остальной код)
+
+            int num_1(double[] arr)
+            {
+                int count = 0;
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i] % 10 == 1 && arr[i] % 7 == 0)
+                    {
+                        count++;
+                    }
+                }
+                return count;
+            }
+
+            // Запрос количества чисел и границ диапазона
+            int num = int.Parse(Console.ReadLine()!);
+            int start = int.Parse(Console.ReadLine()!);
+            int stop = int.Parse(Console.ReadLine()!);
+
+            // Генерация массива случайных чисел
+            double[] mass = GenerateRandomArray(num, start, stop);
+            Print(mass);
+
+            // Подсчёт чисел, оканчивающихся на 1 и делящихся нацело на 7
+            int result = num_1(mass);
+            System.Console.WriteLine(result);
+        }
+
+        // Функция для генерации массива случайных чисел
+        static double[] GenerateRandomArray(int size, int from, int to)
+        {
+            double[] arr = new double[size];
+            Random random = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                arr[i] = random.Next(from, to + 1);
+            }
+            return arr;
+        }
+
+        // Функция для вывода массива
+        static void Print(double[] arr)
+        {
+            foreach (var num in arr)
+            {
+                Console.Write($"{num} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Lesson_4/4_3/Program.cs
@@ -0,0 +1,56 @@
+﻿/*Заполните массив на N (вводится с консоли, не более 8) 
+случайных целых чисел от 0 до 9. 
+Сформируйте целое число, которое будет состоять из цифр из 
+массива. Старший разряд числа находится на 0-м индексе, 
+младший – на последнем. */
+
+using System;
+
+namespace My_4_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Запрос количества чисел в массиве
+            Console.Write("Введите количество чисел в массиве (не более 8): ");
+            int num = int.Parse(Console.ReadLine()!);
+
+            // Проверка, что количество чисел не превышает 8
+            if (num > 8)
+            {
+                Console.WriteLine("Количество чисел не должно превышать 8!");
+                return;
+            }
+
+            // Заполнение массива случайными числами от 0 до 9
+            int[] arr = new int[num];
+            Random random = new Random();
+            Console.Write("Сформированный массив: [ ");
+            for (int i = 0; i < num; i++)
+            {
+                arr[i] = random.Next(0, 10);
+                Console.Write(arr[i]);
+                if (i < num - 1)
+                {
+                    Console.Write(", ");
+                }
+            }
+            Console.WriteLine(" ]");
+
+            // Формирование числа из цифр массива
+            int number = 0;
+            for (int i = 0; i < num; i++)
+            {
+                number += arr[i] * (int)Math.Pow(10, num - i - 1);
+            }
+
+            // Вывод старшего и младшего разрядов числа
+            int firstDigit = number / (int)Math.Pow(10, num - 1);
+            int lastDigit = number % 10;
+            Console.WriteLine($"Сформированное число: {number}");
+            //Console.WriteLine($"Старший разряд числа: {firstDigit}");
+            //Console.WriteLine($"Младший разряд числа: {lastDigit}");
+        }
+    }
+}

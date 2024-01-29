953c9dbebfb27adb7343bda77501f63f30d94fe8
Sergej080295 <milowanow-sergej@mail.ru>
2024-01-29 21:04:19 +0300

    Добавил решение 3 задач по 6 семинару

---
+++ Lesson_6/6_1/Program.cs
@@ -0,0 +1,11 @@
+﻿// 1. Задайте массив символов (тип char []).
+//    Создайте строку из символов этого массива.
+//    Конструктор строки вида string(char []) не использовать.
+
+char[] charArray = { 'H', 'e', 'l', 'l', 'o' };
+string str = "";
+foreach (char i in charArray)
+{
+    str += i;
+}
+Console.WriteLine(str);
---
+++ Lesson_6/6_3/Program.cs
@@ -0,0 +1,13 @@
+﻿/* 3. Считать строку с консоли, состоящую из латинских букв в нижнем регистре. 
+Выяснить, сколько среди введённых букв гласных.*/
+
+string input = Console.ReadLine().ToLower();
+int vowelCount = 0;
+for (int i = 0; i < input.Length; i++)
+{
+    if ("aeiou".Contains(input[i]))
+    {
+        vowelCount++;
+    }
+}
+Console.WriteLine($"Количество гласных букв: {vowelCount}");
